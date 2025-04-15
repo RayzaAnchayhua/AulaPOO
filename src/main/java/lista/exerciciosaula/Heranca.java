6893571db8c76432fd72b0fa80d6eedd7cec5453
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2025-04-15 19:55:47 -0300

    feat:Atualizar o Heranca.java

--- src/main/java/lista/exerciciosaula/Heranca.java
+++ src/main/java/lista/exerciciosaula/Heranca.java
@@ -2,7 +2,9 @@ package lista.exerciciosaula;
 
 public class Heranca {
     public static void main(String[] args) {
-        Eletronicos elet = new Eletronicos();
+Eletronicos elet = new Eletronicos("Notebook",3.500,"Notebook Asus");
+Vestuario vest = new Vestario("Blusa Olivia Rodrigo", 60, "Blusa OR");
+Livros liv = new Livros("Sol da Meia Noite", 60, "Livro");
 
         elet.setMarca("Samsung");
         elet.setModelo("S20+");
